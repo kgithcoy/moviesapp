3bb744d60ee1bcaeacde1c1e6fb847a59086c874
Miquel Stam <mql.stam@gmail.com>
2023-03-29 12:06:20 +0200

    Mealadapter made

---
+++ app/src/main/java/android/moviesapp/movie_item.java
@@ -0,0 +1,14 @@
+package android.moviesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class movie_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_item);
+    }
+}
