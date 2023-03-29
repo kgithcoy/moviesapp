29cb4bad08d82484ae5456ab8aa176a7b003d365
Job79 <job@plabble.org>
2023-03-29 10:31:01 +0200

    chore: add empty android project

---
+++ app/src/main/java/android/moviesapp/MainActivity.java
@@ -0,0 +1,14 @@
+package android.moviesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
