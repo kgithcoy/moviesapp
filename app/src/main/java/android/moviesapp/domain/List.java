c45599a2d6abde49be5c0f3954cdea7d475ccf3a
Job79 <job@plabble.org>
2023-03-29 12:15:41 +0200

    fix: add annotation and setters to database entities

--- app/src/main/java/android/moviesapp/domain/List.java
+++ app/src/main/java/android/moviesapp/domain/List.java
@@ -1,41 +1,69 @@
 package android.moviesapp.domain;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 import java.util.ArrayList;
 
-
-@Entity(tableName = "List_table")
+@Entity(tableName = "list")
 public class List {
+    @PrimaryKey
     private int id;
     private String name;
     private String description;
     private String posterPath;
     private int itemCount;
+
+    @Ignore
     private ArrayList<Movie> items;
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getPosterPath() {
         return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
     }
 
     public int getItemCount() {
         return itemCount;
     }
 
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
     public ArrayList<Movie> getItems() {
         return items;
+    }
+
+    public void setItems(ArrayList<Movie> items) {
+        this.items = items;
     }
 }
 
--- app/src/main/java/android/moviesapp/domain/Movie.java
+++ app/src/main/java/android/moviesapp/domain/Movie.java
@@ -2,11 +2,16 @@ package android.moviesapp.domain;
 
 import androidx.room.Entity;
 
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.ArrayList;
 
-@Entity(tableName = "movie_table")
+@Entity(tableName = "movie")
 public class Movie {
+    @PrimaryKey
     private int id;
+
     private String title;
     private String overview;
     private String posterPath;
@@ -14,46 +19,90 @@ public class Movie {
     private String releaseDate;
     private double voteAverage;
     private int voteCount;
+
+    @Ignore
     private ArrayList<Review> reviews;
+
+    @Ignore
     private ArrayList<Person> cast;
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getOverview() {
         return overview;
     }
 
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
     public String getPosterPath() {
         return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
     }
 
     public String getBackdropPath() {
         return backdropPath;
     }
 
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
     public String getReleaseDate() {
         return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
     }
 
     public double getVoteAverage() {
         return voteAverage;
     }
 
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
     public int getVoteCount() {
         return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
     }
 
     public ArrayList<Review> getReviews() {
         return reviews;
     }
 
+    public void setReviews(ArrayList<Review> reviews) {
+        this.reviews = reviews;
+    }
+
     public ArrayList<Person> getCast() {
         return cast;
+    }
+
+    public void setCast(ArrayList<Person> cast) {
+        this.cast = cast;
     }
 }
