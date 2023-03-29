318db51d726f55fcc0d77a0892982649eeb69f38
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-03-29 10:48:26 +0200

    refactor: move MainActivity to presentation package

--- app/src/main/java/android/moviesapp/presentation/MainActivity.java
+++ app/src/main/java/android/moviesapp/presentation/MainActivity.java
@@ -1,7 +1,8 @@
-package android.moviesapp;
+package android.moviesapp.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.moviesapp.R;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
